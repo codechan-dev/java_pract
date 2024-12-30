25af34ccb3bd9dd709b00e6d07aa6ec0fbae1f6a
codechan-dev <officialchandru87@gmail.com>
2024-12-30 19:16:40 +0530

    fruits arrays

--- Hello.java
+++ Hello.java
@@ -1,13 +1,16 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 // import javax.swing.JOptionPane;
 public class Hello {
 
     public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
-        char s='@';
-        if(s =='@'){
-            System.out.println("its true");
-        }
+       ArrayList<String> fruits = new ArrayList<>();
+       fruits.add("apple");
+       fruits.add("banana");
+       fruits.add("orange");
+       for (String string : fruits) {
+        System.out.print(string +' ');
+       }
     }
 } 
