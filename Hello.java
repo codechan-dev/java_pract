63dc7b4c895e0c7551fed6b2d50b7accfc7b3b27
codechan-dev <officialchandru87@gmail.com>
2024-12-30 18:50:49 +0530

    y6tfyf

---
+++ Hello.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+// import javax.swing.JOptionPane;
+public class Hello {
+
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        int a,b;
+        a=scan.nextInt();
+        switch (a) {
+            case 1:
+            System.out.println("you pressed 1");                
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
