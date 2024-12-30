9a771005e4a60c701e3deba7996ebf778426537e
codechan-dev <officialchandru87@gmail.com>
2024-12-30 19:02:16 +0530

    code deleted

--- Hello.java
+++ Hello.java
@@ -5,15 +5,6 @@ public class Hello {
 
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
-        int a,b;
-        a=scan.nextInt();
-        switch (a) {
-            case 1:
-            System.out.println("you pressed 1");                
-                break;
         
-            default:
-                break;
-        }  
     }
 } 
